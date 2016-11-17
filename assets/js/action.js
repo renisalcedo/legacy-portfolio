ffb134d200c885f0f78c11a59f7c4a7b9536e276
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2016-11-17 13:26:01 -0500

    Added waypoint library and cleaned code

--- assets/js/action.js
+++ assets/js/action.js
@@ -2,7 +2,9 @@ $(function() {
   var toggled = 0;
   var header = $('header');
   var aside = $('#sidebar');
+  // Target the first h2 in the header
   var iam = $('#iAm');
+  // Qualities display on the header
   var attr = $('#attr');
   var qualities = ['Front-End Developer', 'Back-End Developer', 'Full-Stack Developer', 'Student', 'Designer'];
 
@@ -11,6 +13,7 @@ $(function() {
   var colLess = 'col-xs-10 col-sm-10 col-md-10 col-lg-10';
 
   var emailMe = function(id) {
+    // When click will replace the amount of space depending on the need
     $(id).click(function() {
       if(toggled === 1) {
         header.removeClass(colLess);
@@ -32,6 +35,7 @@ $(function() {
 
   // Attributes display
   var i = 0;
+  // Iterate through the values in the array of qualities
   var interval = setInterval(function() {
     i++;
     if(i == qualities.length) {
@@ -39,4 +43,6 @@ $(function() {
     }
     attr.text(qualities[i]);
   }, 1500);
+
+  // Waypoint functionality
 });
